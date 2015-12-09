51dc3d89943da99b5945bd76b6068e2a4c566ca9
yanlongtao <longtao.yan@zymobi.com>
2015-12-09 14:34:39 +0800

    添加设置编码格式的接口

--- src/org/zywx/wbpalmstar/plugin/uexsocketmgr/MulticastMgr.java
+++ src/org/zywx/wbpalmstar/plugin/uexsocketmgr/MulticastMgr.java
@@ -10,24 +10,24 @@ import android.util.Log;
  * android:name="android.permission.CHANGE_WIFI_MULTICAST_STATE" /> 权限
  */
 public class MulticastMgr {
-	private static final String TAG = "MulticastMgr";
-	private static MulticastLock multicastLock;
+    private static final String TAG = "MulticastMgr";
+    private static MulticastLock multicastLock;
 
-	public static void allowMulticast(Context context) {
-		Log.i(TAG, "allowMulticast");
-		WifiManager wifiManager = (WifiManager) context
-				.getSystemService(Context.WIFI_SERVICE);
-		multicastLock = wifiManager
-				.createMulticastLock("multicast.uexSocketMgr");
-		multicastLock.acquire();
-	}
+    public static void allowMulticast(Context context) {
+        Log.i(TAG, "allowMulticast");
+        WifiManager wifiManager = (WifiManager) context
+                .getSystemService(Context.WIFI_SERVICE);
+        multicastLock = wifiManager
+                .createMulticastLock("multicast.uexSocketMgr");
+        multicastLock.acquire();
+    }
 
-	public static void disableMulticast() {
-		Log.i(TAG, "disableMulticast");
-		if (multicastLock == null) {
-			return;
-		}
-		multicastLock.release();
-		multicastLock = null;
-	}
+    public static void disableMulticast() {
+        Log.i(TAG, "disableMulticast");
+        if (multicastLock == null) {
+            return;
+        }
+        multicastLock.release();
+        multicastLock = null;
+    }
 }
